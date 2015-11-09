7124872b1e55e4e9847a92303472cd57b89e4a1a
lei xinye <xinyelei@lawn-128-61-43-47.lawn.gatech.edu>
2015-11-09 16:00:16 -0500

    Initial Commit

---
+++ MVC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MVC
+//
+//  Created by xinye lei on 15/11/9.
+//  Copyright © 2015年 xinye lei. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
